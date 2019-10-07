5c68105a8e6f06312e12714a763b9ead579a2ec3
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2019-10-07 16:37:17 +0200

    C++ könyv Esettan
    DE - IK prog2 example

---
+++ C++-Examples/esettan/computerproductfactory.h
@@ -0,0 +1,12 @@
+
+#ifndef COMPUTERPRODUCTFACTORY_H
+#define COMPUTERPRODUCTFACTORY_H
+
+#include "productfactory.h"
+
+class ComputerProductFactory: public ProductFactory {
+public:
+    Product* createProduct(char typeCode) const;
+};
+
+#endif // COMPUTERPRODUCTFACTORY_H
